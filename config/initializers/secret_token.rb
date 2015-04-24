f56e882682147af89b8011e147ad13e759ae09f7
Juraci <juraci.vieira@gmail.com>
2015-04-24 19:50:23 -0300

    first commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+ZombieBackend::Application.config.secret_token = '833cd39f0a29567a0cd5a39a45bd25c54d847855f3a99db7a2caaf6f374752ef66cd479bfde0b34042056aab52d54e43afcfb897cd9eb959782cbf2a32573dec'
